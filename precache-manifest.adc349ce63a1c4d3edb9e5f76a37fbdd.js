6cdef661bf85ca6d984a026ef1e6cb87b2184c9c
Michail <mikbox74@gmail.com>
2020-01-14 19:13:24 +0300

    Updates

---
+++ precache-manifest.adc349ce63a1c4d3edb9e5f76a37fbdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ba8292c986fc83a2c2b976203b75a1",
+    "url": "/tasknik/index.html"
+  },
+  {
+    "revision": "da1d9acb3efaf0216f83",
+    "url": "/tasknik/static/css/main.a0e01824.chunk.css"
+  },
+  {
+    "revision": "299c43ab8138df75d6c7",
+    "url": "/tasknik/static/js/2.22e78c22.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tasknik/static/js/2.22e78c22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da1d9acb3efaf0216f83",
+    "url": "/tasknik/static/js/main.0bb60217.chunk.js"
+  },
+  {
+    "revision": "aba21b02f479bc1dd8ca",
+    "url": "/tasknik/static/js/runtime-main.6a46d7b9.js"
+  }
+]);
